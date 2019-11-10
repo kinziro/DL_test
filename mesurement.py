67ee3b1967b8d6fbe4aae1a41930909e7e2e0565
kinziro <kazushi@MacBook.local>
2019-11-10 22:44:29 +0900

    修正

--- mesurement.py
+++ mesurement.py
@@ -38,15 +38,31 @@ def make_env(env_name, rank, seed=0):
 class MyRemodelendEnv(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
+        self.env.reset()
+        self.status_name = ['z-self.initial_z', 'np.sin_self.angel_to_target', 'np.cos_self.angel_to_target',
+                            '0.3vx', '0.3vy', '0.3vz', 'r', 'p']
+
+        for jnt in self.env.ordered_joints:
+            self.status_name.append('{}_p'.format(jnt.name))
+            self.status_name.append('{}_v'.format(jnt.name))
+
+        self.status_name.extend(['feet_contact_0', 'feet_contact_1'])
+        np.save('./humanoid_labels', np.array(self.status_name))
 
     def step(self, a):
+        '''
+        instance = self.env.ordered_joints[0]
 
-        x = self.env.ordered_joints[0].current_position()
+        for method in dir(instance):
+            if callable(getattr(instance, str(method))):
+                print(method)
+        '''
 
-        origin_value = np.array([j2.current_position() for j2 in self.env.ordered_joints], dtype=np.float32).flatten()
+        #origin_value = np.array([j2.current_position() for j2 in self.env.ordered_joints], dtype=np.float32).flatten()
+        #origin_value = [0]*34
         obs, reward, terminal, info = self.env.step(a)
 
-        return obs, reward, terminal, origin_value
+        return obs, reward, terminal, info
 
 
 #import myenv
@@ -78,15 +94,20 @@ test_env = gym.make(env_name)
 test_env = MyRemodelendEnv(test_env)
 test_env = DummyVecEnv([lambda: test_env])
 
+plotdir = './testplot/'
+os.makedirs(plotdir, exist_ok=True)
+data = None
+step_len = 1000
 # 学習結果の確認
 if validation:
     model = PPO2.load('{}ppo2_model'.format(savedir))
+    from gym import wrappers
 
     done = False
     #obs = env.reset()
     obs = test_env.reset()
 
-    for step in range(1):
+    for step in range(step_len):
         if done:
             time.sleep(1)
             o = test_env.reset()
@@ -94,9 +115,65 @@ if validation:
 
         action, _states = model.predict(obs)
         obs, rewards, done, info = test_env.step(action)
-        print(obs.shape)
-        print(obs)
 
+        if data is None:
+            data = np.array(obs).reshape(-1, 1)
+        else:
+            data = np.hstack([data, np.array(obs).reshape(-1, 1)])
+        print(step, obs.shape, data.shape)
+
+
+    labels = np.load('./humanoid_labels.npy')
+
+    '''
+    for i in range(data.shape[0]):
+        plt.clf()
+        plt.plot(data[i, :])
+        plt.grid()
+        title = info[0][i]
+        plt.title(title)
+        plt.savefig('{}{}.png'.format(plotdir, title))
+    '''
+
+
+    fig_num = 7
+    x = list(range(data.shape[1]))
+    fig = plt.figure(figsize=(len(x)/20, fig_num*2))
+
+    title = 'angle'
+    plt.title(title)
+    ax1 = fig.add_subplot(fig_num, 1, 1)
+    ax1.plot(x, data[10, :])
+    ax1.grid()
+    ax1.set_ylabel(labels[10])
+    ax2 = fig.add_subplot(fig_num, 1, 2)
+    ax2.plot(x, data[18, :])
+    ax2.grid()
+    ax2.set_ylabel(labels[18])
+    ax3 = fig.add_subplot(fig_num, 1, 3)
+    ax3.plot(x, data[20, :])
+    ax3.grid()
+    ax3.set_ylabel(labels[20])
+    ax4 = fig.add_subplot(fig_num, 1, 4)
+    ax4.plot(x, data[26, :])
+    ax4.grid()
+    ax4.set_ylabel(labels[26])
+    ax5 = fig.add_subplot(fig_num, 1, 5)
+    ax5.plot(x, data[28, :])
+    ax5.grid()
+    ax5.set_ylabel(labels[28])
+    ax6 = fig.add_subplot(fig_num, 1, 6)
+    ax6.plot(x, data[42, :])
+    ax6.grid()
+    ax6.set_ylabel(labels[42])
+    ax7 = fig.add_subplot(fig_num, 1, 7)
+    ax7.plot(x, data[43, :])
+    ax7.grid()
+    ax7.set_ylabel(labels[43])
+    plt.savefig('{}{}.png'.format(plotdir, title))
+
+
+    '''
     data = info[0]
 
     fig = plt.figure()
@@ -120,6 +197,7 @@ if validation:
     ax.legend()
 
     plt.show()
+    '''
 
 
 env.close()
